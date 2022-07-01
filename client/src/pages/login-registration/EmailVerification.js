25a7a2078f1bb54007fe092f9fb76b59e6d03db3
Prateek Khindri <prateek.khindri90@gmail.com>
2022-07-01 22:40:23 +1000

    Added email verification

--- client/src/pages/login-registration/EmailVerification.js
+++ client/src/pages/login-registration/EmailVerification.js
@@ -1,23 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MainLayout } from "../../layout/MainLayout";
 import { useSearchParams } from "react-router-dom";
 import { emailVerificationAdminUser } from "../../helpers/axiosHelper";
+import { Alert, Spinner } from "react-bootstrap";
 
 const EmailVerification = () => {
   let [searchParams] = useSearchParams();
   // console.log(searchParams.get("e"));
   // console.log(searchParams.get("c"));
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [response, setResponse] = useState({});
+
   useEffect(() => {
     const email = searchParams.get("e");
     const verificationCode = searchParams.get("c");
 
-    emailVerificationAdminUser({ email, verificationCode });
+    submitVerificationCode({ email, verificationCode });
   }, []);
+
+  const submitVerificationCode = async (obj) => {
+    setIsLoading(true);
+    const result = await emailVerificationAdminUser(obj);
+    setIsLoading(false);
+    setResponse(result);
+  };
 
   return (
     <MainLayout>
-      <h1>EmailVerification to do</h1>
+      <div className="verification mt-5 pt-5">
+        <div className="message">
+          <h1>Email verification</h1>
+          {response?.message && (
+            <Alert
+              variant={response.status === "success" ? "success" : "danger"}
+            >
+              {" "}
+              {response?.message}
+            </Alert>
+          )}
+          {/* <Alert variant="success">{}</Alert> */}
+          {isLoading && <Spinner variant="primary" animation="border" />}
+        </div>
+      </div>
     </MainLayout>
   );
 };
