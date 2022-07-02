7452934cf76bdee8e2285ceb656ee7c74c796a41
Prateek Khindri <prateek.khindri90@gmail.com>
2022-07-02 11:40:01 +1000

    Updated LoginForm on the client side

--- client/src/components/login form/LoginForm.js
+++ client/src/components/login form/LoginForm.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [form, setForm] = useState({});
+
+  const handleOnChange = (e) => {
+    // console.log(e);
+
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          name="email"
+          onChange={handleOnChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleOnChange}
+          required
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+  );
 };
