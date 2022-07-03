bc51bc264f8a7d1cdb1a506fdb58ec5fdb11c623
Prateek Khindri <prateek.khindri90@gmail.com>
2022-07-03 13:01:53 +1000

    Updated the sideMenu

--- client/src/components/side-menu/SideMenu.js
+++ client/src/components/side-menu/SideMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ListGroup } from "react-bootstrap";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { useDispatch, useSelector } from "react-redux";
@@ -22,49 +22,73 @@ export const SideMenu = () => {
 
         <Offcanvas.Body>
           <ListGroup variant="flush">
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/dashboard" className="nav-link">
                 <i class="fa-solid fa-gauge"></i> Dashboard
               </Link>
             </ListGroup.Item>
 
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/categories" className="nav-link">
                 Categories
               </Link>
             </ListGroup.Item>
 
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/products" className="nav-link">
                 Products
               </Link>
             </ListGroup.Item>
 
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/payment-method" className="nav-link">
                 Payment Method
               </Link>
             </ListGroup.Item>
 
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/users" className="nav-link">
                 Users
               </Link>
             </ListGroup.Item>
 
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/orders" className="nav-link">
                 Orders
               </Link>
             </ListGroup.Item>
 
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/profile" className="nav-link">
                 Profile
               </Link>
             </ListGroup.Item>
 
-            <ListGroup.Item className="fs-5">
+            <ListGroup.Item
+              className="fs-5"
+              onClick={() => dispatch(toggleShowSideMenu())}
+            >
               <Link to="/settings" className="nav-link">
                 Settings
               </Link>
